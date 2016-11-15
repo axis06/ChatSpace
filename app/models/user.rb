53a4f7ef129ac3bd9a6ec13011104a6ab7017926
axis06 <withcomputer@gmail.com>
2016-11-15 23:17:19 +0900

    nameテストコード追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   has_many :groups, through: :groupusers
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  validates :name, presence: true
 end
--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,1 +1,10 @@
 require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "is invalid without a nickname" do
+      user = User.new(name: "", email: "admin@example.com", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:name]).to include("名前に空白は利用できません")
+    end
+  end
+end
