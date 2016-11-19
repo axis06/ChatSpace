48110085073cfa156d68df4dd2ac92bec8a88f7e
axis06 <withcomputer@gmail.com>
2016-11-19 14:55:08 +0900

    ルーティングの変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :messages, only: [:create]
+  resources :groups, only: [:new,:create,:edit,:update]
   get 'home/index'
   root 'home#index'
 end
