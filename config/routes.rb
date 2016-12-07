4f8acf9371948a2dad5a22b0671df551ea8d0279
axis06 <withcomputer@gmail.com>
2016-12-07 19:31:12 +0900

    検索用のAPIの実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :groups, except: [:delete] do
     resources :messages, only: [:create,:show]
   end
+  get '/users/search/:name', to: 'users#search'
   get 'home/index'
   root 'home#index'
 end
